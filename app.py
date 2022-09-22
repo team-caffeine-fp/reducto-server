bab90268d35e64bc38d5860cd13349314035a541
Mikegit config --global user.email mikmercieca13@gmail.com <101655537+Mike-Mercieca@users.noreply.github.com>
2022-09-22 16:05:43 +0100

    setup files and initial routes

---
+++ app.py
@@ -0,0 +1,31 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    return "hello, welcome to reducto server"
+
+@app.route('/dashboard/', methods=['GET'])
+def dashboard():
+    return "I don't think we actually need this"
+
+# Not finished
+@app.route('/login/', methods=['POST'])
+def login():
+    return "thanks for logging in"
+
+# Not finished
+@app.route('/register/', methods=['POST'])
+def register():
+    return "Thanks for registering"
+
+# Not finished
+@app.route('/form/', methods=['POST'])
+def form():
+    return "Thanks for letting us know your carbon footprint"
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
